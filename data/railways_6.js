8938d9e6982e55cca12fe5b90a8d03663a033afd
marwanaied9999 <119815927+marwanaied9999@users.noreply.github.com>
2022-12-06 01:27:06 +0300

    Add files via upload

---
+++ data/railways_6.js
@@ -0,0 +1,9 @@
+var json_railways_6 = {
+"type": "FeatureCollection",
+"name": "railways_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "osm_id": "206373167", "name": "Red Line Metro", "type": "subway" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 55.2642941, 25.1954972 ], [ 55.2600944, 25.190986 ] ] ] } },
+{ "type": "Feature", "properties": { "osm_id": "700726320", "name": "Red Line Metro", "type": "subway" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 55.2643231, 25.1954664 ], [ 55.2606871, 25.1915544 ] ] ] } }
+]
+}
